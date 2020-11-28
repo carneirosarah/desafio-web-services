1a9896be6435f481fddc25656b0cd72a542b84a0
carneirosarah <sarahrlcarneiro@gmail.com>
2020-11-28 20:25:29 -0300

    Implementação do layout splash screen

--- app/src/main/java/com/example/desafio_web_services/ui/SplashActivity.kt
+++ app/src/main/java/com/example/desafio_web_services/ui/SplashActivity.kt
@@ -1,12 +1,28 @@
 package com.example.desafio_web_services.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafio_web_services.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
+
+    val scope = CoroutineScope(Dispatchers.Main)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        scope.launch {
+
+            delay(5000)
+            val intent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
