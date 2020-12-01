6121e6f37a2976eebacd1d9d6379d0887bd154f1
carneirosarah <sarahrlcarneiro@gmail.com>
2020-12-01 20:11:21 -0300

    layout tela de cadastro

--- app/src/main/java/com/example/desafio_web_services/ui/CadastroActivity.kt
+++ app/src/main/java/com/example/desafio_web_services/ui/CadastroActivity.kt
@@ -1,12 +1,21 @@
 package com.example.desafio_web_services.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.desafio_web_services.R
+import kotlinx.android.synthetic.main.activity_cadastro.*
 
 class CadastroActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro)
+
+        cadastroButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        cadastroAppBar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
